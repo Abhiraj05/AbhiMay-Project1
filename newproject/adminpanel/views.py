3b28db788378414f6cb5fe5a1fa0b25971c10438
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-09-30 20:15:41 +0530

    user registration model created

--- newproject/adminpanel/views.py
+++ newproject/adminpanel/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from django.contrib import messages
+from adminpanel.models import *
 
 # Create your views here.
 def main(request):
     return render(request,"main.html")
 
+def value_check(value):
+    return value == "" or value is None
+
+def user_exist(username,password):
+    user=User_Registration(username=username,password=password).first()
+    return user
+
 def login(request):
+    if request.method  == "POST":
+        username=username.POST.get("")
+        password=password.POST.get("")
+        confirm_password=confirm_password.POST.get("")
+        
+        if value_check(username):
+            messages.warning("please the enter the username")
+            
+        if value_check(password):
+            messages.warning("please the enter the password")
+            
+        if password!=confirm_password:
+            messages.warning("password is not matching")
+        
+        if user_exist(username,password):
+            messages.warning("user not register")
+        
+        
     return render(request,"login.html")
